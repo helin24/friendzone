8842cf3da6e0d78508c4c96744d6bdb9acc13585
Zach Taylor <taylorzr@gmail.com>
2014-10-23 11:51:15 -0500

    Renames user_id to participator_id

--- db/migrate/20141022221206_create_participations.rb
+++ db/migrate/20141022221206_create_participations.rb
@@ -2,7 +2,7 @@ class CreateParticipations < ActiveRecord::Migration
   def change
     create_table :participations do |t|
       t.integer :activity_id
-      t.integer :user_id
+      t.integer :participator_id
       t.boolean :accepted
 
       t.timestamps
